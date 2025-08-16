0cf6b98723cfa84645a2a75a79c3be04236d4613
ellisn19 <112054165+ellisn19@users.noreply.github.com>
2025-08-15 23:59:35 -0400

    Create Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		string targetFolder = @"G:\iPhonePictures2";
+		var transfer = new IPhoneMediaTransfer();
+		transfer.CopyAllMedia(targetFolder);
+		Console.WriteLine("Transfer complete!");
+	}
+}
