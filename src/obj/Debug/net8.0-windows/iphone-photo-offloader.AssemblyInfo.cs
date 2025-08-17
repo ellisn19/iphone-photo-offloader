b54a854ab7c35505ebe2ea2c308a09e5fdfd2c5d
ellisn19 <112054165+ellisn19@users.noreply.github.com>
2025-08-17 11:32:55 -0400

    bin/obj

--- src/obj/Debug/net8.0-windows/iphone-photo-offloader.AssemblyInfo.cs
+++ src/obj/Debug/net8.0-windows/iphone-photo-offloader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iphone-photo-offloader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3fb54d50c8ef04198de44493d9d6aacf8eec9ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c82fbb661295adec58232ec87c7a889a6b6bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("iphone-photo-offloader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iphone-photo-offloader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
